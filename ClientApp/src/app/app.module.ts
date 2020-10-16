7d62c0e71f7f33f14b5003f0d9ad80505ebfe025
Gabriel <creeplays@gmail.com>
2020-10-15 22:28:50 -0300

    Icones e menu lateral.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import icons from '../assets/svg/svg-icons';
+import { SvgIconsModule } from '@ngneat/svg-icon';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    SvgIconsModule.forRoot({
+      icons
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  styleUrls: ['./nav-menu.component.scss']
 })
 export class NavMenuComponent {
   isExpanded = false;
+  selectedSideNavigationStyle="";
+  arrowStyle = "";
+  categories : Array<{name:string,icon:string,type:string,size?:string}>= 
+  [
+    {name:"Surf",icon:"surf",type:"image",size:"60px"},
+    {name:"Skateboard",icon:"skateboard",type:"image",size:"60px"},
+    {name:"StandUp Paddle",icon:"standUpPaddle",type:"image",size:"60px"},
+    {name:"Barcos",icon:"motorboat",type:"image",size:"60px"},
+    {name:"Jet-ski",icon:"jetSki",type:"image",size:"60px"},
+  ]
 
   collapse() {
     this.isExpanded = false;
@@ -14,5 +25,17 @@ export class NavMenuComponent {
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+  sideNavToggle(){
+    if(this.selectedSideNavigationStyle == "")
+    {
+      this.selectedSideNavigationStyle = "lateral-navigation-open"
+      this.arrowStyle = "lateral-icon-open"
+    }
+    else
+    {
+      this.selectedSideNavigationStyle = ""
+      this.arrowStyle = ""
+    }
   }
 }
