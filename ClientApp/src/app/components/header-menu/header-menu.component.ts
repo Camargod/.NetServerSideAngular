6d4854f22f2f6bcfeede1942612e3db1cdd48240
Gabriel <creeplays@gmail.com>
2021-05-02 20:25:33 -0300

    feat(mar-10-mar9): Ajustes gerais na pagina. Reestilização de componentes e novo esquema de UI/UX
    Co-authored-by: Matheus Braz de Oliveira <theus.braz18@gmail.com>

--- ClientApp/src/app/components/header-menu/header-menu.component.ts
+++ ClientApp/src/app/components/header-menu/header-menu.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header-menu',
--- ClientApp/src/app/components/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/components/nav-menu/nav-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav-menu',
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 })
 export class NavMenuComponent {
   // isExpanded = false;
-  arrowStyle = "";
+  @ViewChild('navmenu') navMenuDiv : ElementRef<HTMLDivElement>; 
   categories : Array<{id:string,name:string,icon:string,type:string,size?:string}>= 
   [
     {id:"surf",name:"Surf",icon:"surf",type:"image",size:"60px"},
@@ -17,10 +17,44 @@ export class NavMenuComponent {
     {id:"jetski",name:"Jet-ski",icon:"jetSki",type:"image",size:"60px"},
   ]
 
-  constructor(private router : Router){
+  isOpenMobile = false;
+  isSizedMobile = false;
+
+  constructor(private router : Router, private renderer: Renderer2){
   }
 
   navigateCategory(category){
     this.router.navigate(["category",category.id])
+  }
+  openMenuMobileClick(){
+    !this.isOpenMobile ? this.menuOpen() : this.menuClose();  
+  }
+
+  menuOpen(){
+    this.isOpenMobile = true;
+    this.renderer.setStyle(this.navMenuDiv.nativeElement,"display","flex"); 
+  }
+
+  menuClose(){
+    this.isOpenMobile = false;
+    this.renderer.setStyle(this.navMenuDiv.nativeElement,"display","none");
+  }
+
+  @HostListener("window:resize", ['$event'])
+  onWindowResize(event){
+    let {innerWidth} = event.srcElement;
+    if(innerWidth >= 1024){
+      if(this.isSizedMobile){
+        this.menuOpen(); 
+        this.isSizedMobile = false;
+
+      }
+    }
+    else{
+      if(!this.isSizedMobile){
+        this.menuClose(); 
+        this.isSizedMobile = true
+      }
+    } 
   }
 }
